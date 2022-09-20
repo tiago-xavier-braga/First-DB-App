dfc8b110a3be2f7e51d3a25f1ca6e28cb8779907
Tiago Braga <braga.tiagoxavier@gmail.com>
2022-09-20 19:40:41 -0300

    Modified js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,38 @@
-var db;
-var request = indexedDB.open("DBteste");
+const DadosClientes = [
+  { ssn: "444-44-4444", nome: "Bill", idade: 35, email: "bill@company.com" },
+  { ssn: "555-55-5555", nome: "Donna", idade: 32, email: "donna@home.org" }
+];
+const dbName = "clientes";
+
+var request = indexedDB.open(dbName, 2);
+
 request.onerror = function(event) {
-  alert("Você não habilitou minha web app para usar IndexedDB?!");
+  // Tratar erros.
 };
-request.onsuccess = function(event) {
-  db = request.result;
+request.onupgradeneeded = function(event) {
+  var db = event.target.result;
+
+  // Cria um objectStore para conter a informação sobre nossos clientes. Nós vamos
+  // usar "ssn" como key path porque sabemos que é único;
+  var objectStore = db.createObjectStore("clientes", { keyPath: "ssn" });
+
+  // Cria um índice para buscar clientes pelo nome. Podemos ter nomes
+  // duplicados, então não podemos usar como índice único.
+  objectStore.createIndex("nome", "nome", { unique: false });
+
+  // Cria um índice para buscar clientes por email. Queremos ter certeza
+  // que não teremos 2 clientes com o mesmo e-mail;
+  objectStore.createIndex("email", "email", { unique: true });
+
+  // Usando transação oncomplete para afirmar que a criação do objectStore
+  // é terminada antes de adicionar algum dado nele.
+  objectStore.transaction.oncomplete = function(event) {
+    // Armazenando valores no novo objectStore.
+    var clientesObjectStore = db.transaction("clientes", "readwrite").objectStore("clientes");
+    for (var i in DadosClientes) {
+      clientesObjectStore.add(DadosClientes[i]);
+    }
+  }
+  var transaction = db.transaction(["clientes"], "readwrite");
+  console.log(transaction)
 };
